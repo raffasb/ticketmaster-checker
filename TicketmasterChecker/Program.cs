d9bc72b24c7ee06211ef4d2bba3b421636015ef0
Rafael Barros dos Santos <r.barros.dos.santos@avanade.com>
2024-10-18 11:49:12 -0300

    Solution items

---
+++ TicketmasterChecker/Program.cs
@@ -0,0 +1,23 @@
+﻿using TicketmasterChecker;
+
+Console.WriteLine($"""
+            ========================================================
+            Starting the Ticketmaster Checker at {DateTime.Now}
+            ========================================================
+            """
+);
+
+new TicketmasterCore("https://www.ticketmaster.com.br/event/venda-geral-linkin-park", "Linkin Park - 15/11")
+    .DoWork()
+    .Wait();
+
+//new TicketmasterCore("https://www.ticketmaster.com.br/event/venda-geral-linkin-park-extra", "Linkin Park - 16/11")
+//    .DoWork()
+//    .Wait();
+
+Console.WriteLine($"""
+            ========================================================
+            Ending the Ticketmaster Checker at {DateTime.Now}
+            ========================================================
+            """
+);
---
+++ TicketmasterChecker/TicketmasterCore.cs
@@ -0,0 +1,70 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TicketmasterChecker;
+
+internal sealed class TicketmasterCore
+{
+    private readonly string _url;
+    private readonly string _urlDescription;
+    private readonly IWebDriver _webDriver;
+
+    private const int INTERATION_INTERVAL = 10_000;
+    private const int BEEP_FREQUENCY = 1_000;
+    private const int BEEP_DURATION = 5_000;
+
+    private const string AVAILABILITY_KEYWORD_REFERENCE = "Esgotado";
+    private const string AVAILABILITY_OPTIONS_ELEMENT_ID = "show-button";
+    private const string AVAILABILITY_VALUES_ELEMENT_ID = "66923";
+
+    public TicketmasterCore(string url, string urlDescription)
+    {
+        _url = url;
+        _urlDescription = urlDescription;
+
+        var options = new ChromeOptions();
+        options.AddArgument("headless");
+
+        _webDriver = new ChromeDriver(options);
+    }
+
+    public async Task DoWork()
+    {
+        while (true)
+        {
+            Console.WriteLine($"Checking tickets... {DateTime.Now} ({_urlDescription})");
+
+            try
+            {
+                await _webDriver.Navigate().GoToUrlAsync(_url);
+
+                _webDriver.FindElement(By.Id(AVAILABILITY_OPTIONS_ELEMENT_ID))?.Click();
+
+                var elementValue = _webDriver.FindElement(By.Id(AVAILABILITY_VALUES_ELEMENT_ID))?.Text;
+
+                if (IsThereAnyTicketAvailable(elementValue))
+                {
+                    NotifyUser();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred ({_urlDescription}): {ex.Message}");
+            }
+
+            await Task.Delay(INTERATION_INTERVAL);
+        }
+    }
+
+    private bool IsThereAnyTicketAvailable(string? elementValue)
+    {
+        return !string.IsNullOrEmpty(elementValue) && !elementValue.Contains(AVAILABILITY_KEYWORD_REFERENCE, StringComparison.InvariantCultureIgnoreCase);
+    }
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "Known that is only a Windows capability")]
+    private void NotifyUser()
+    {
+        Console.WriteLine($"Tickets available at {DateTime.Now} ({_urlDescription})");
+        Console.Beep(BEEP_FREQUENCY, BEEP_DURATION);
+    }
+}
